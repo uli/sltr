6282c36b3c967b0d88e0a2bded6cef5a1d2f3170
Ulrich Hecht <uli@fpond.eu>
2026-06-19 14:12:37 +0200

    cliptags.sh: add enum support

--- cliptags.sh
+++ cliptags.sh
@@ -13,12 +13,24 @@
 tags="$1"
 shift
 path="$(dirname "$tags")"
-while IFS=':' read typ tag ; do
+
+function gettag() {
+    typ="$1"
+    tag="$2"
     readtags -t "$tags" -e -n "$tag"|while IFS=$'\t' read t file misc ; do
+        if [[ "$misc" =~ enum: ]] ; then
+            enum="${misc/*enum:/}"
+            gettag e "$enum"
+            return
+    	fi
     	start="$(echo "$misc"|sed 's@^.*line:\([0-9]*\).*$@\1@')"
     	end="$(echo "$misc"|sed 's@^.*end:\([0-9]*\)$@\1@')"
     	#echo "$file" $start $end
     	echo // "$file":
     	sed -n "${start},${end}p" "$path"/"$file" 2>/dev/null
     done
+}
+
+while IFS=':' read typ tag ; do
+	gettag "$typ" "$tag"
 done
